27cec67ada22bc2f1febc47c411e35f07d56ba4e
Mehret Kulala <merutaye@gmail.com>
2018-03-03 10:09:32 -0600

    added index

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log('Hello');
+
+var favAnimal = {
+    name: "zoe",
+    animal: "cat"
+};
+
+console.log("favAnimal", favAnimal.animal);
+// console.log("favAnimal", favAnimala['animal']);//other way of console.loging an object
+
+
+var instructorPets = [
+    {
+        insturctorName: "Zoe",
+        petName: "Biff",
+        animalType: "evil cat",
+        isFavorite: true
+    },
+
+    {
+        insturctorName: "Callan",
+        petName: "Seymour",
+        animalType: "one eyed dog",
+        isFavorite: true
+    },
+
+    {
+        insturctorName: "Callan",
+        petName: "Wiley",
+        animalType: "destructive dog",
+        isFavorite: false
+    },
+
+];
+console.log("insturctorPets:" ,instructorPets);
